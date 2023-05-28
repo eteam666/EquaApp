bce4a04f1acbb6f8e78b05e1d982f1eeb9d69ce9
eteam666 <122675543+eteam666@users.noreply.github.com>
2023-05-28 21:01:17 +0800

    r

--- components/Hero.vue
+++ components/Hero.vue
@@ -9,6 +9,8 @@
           <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">EquaApps</h1>
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <a href="/project" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">查看所有项目</a>
+            <a href="https://status.equaapps.net/status/simple" class="rounded-md bg-white-600 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">查看服务状态</a>
+
           </div>
         </div>
       </div>
